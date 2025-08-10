3bcd7f5ac5b198c7b16e91f1c02cc897c9bbf49c
candy <3263554644@qq.com>
2025-08-10 12:59:36 +0800

    1

--- ZoeWorld/ZoeWorld/Program.cs
+++ ZoeWorld/ZoeWorld/Program.cs
@@ -18,7 +18,7 @@ namespace ZoeWorld
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ImageExchange());
+            Application.Run(new VlcPlayerForm());
         }
     }
 }
